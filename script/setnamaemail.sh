8c8c25a8bede1c398ab76ad22f5785266a5ac0d4
otm <oektomo@gmail.com>
2017-04-18 15:17:18 +0700

    adding setnamaemail.sh to set name and email in local repository

---
+++ script/setnamaemail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#cat ~/.gitconfig
+# remove this entry so every one of them has to tell who they are
+# look for command to remove user.email and user.name globally
+git config user.email $2
+git config user.name $1
+git config user.email 
+git config user.name 
