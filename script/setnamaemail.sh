13e0ed58afc377789c37de211b989e28160e4cb2
oetomo <oektomo@gmail.com>
2017-04-18 15:25:18 +0700

    remove global name and email if any

--- script/setnamaemail.sh
+++ script/setnamaemail.sh
@@ -2,6 +2,9 @@
 #cat ~/.gitconfig
 # remove this entry so every one of them has to tell who they are
 # look for command to remove user.email and user.name globally
+git config --global --unset-all user.name
+git config --global --unset-all user.email
+
 git config user.email $2
 git config user.name $1
 git config user.email 
